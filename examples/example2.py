63effefb2f41c7bdbba91a681ab450408d716b07
Leo <lauersperg@gmx.at>
2020-06-18 16:14:13 +0200

    21.) Cleanup example2

--- examples/example2.py
+++ examples/example2.py
@@ -13,12 +13,16 @@ class MainApp(App):
     """
 
     def build(self):
+        root = FloatLayout()
+        renderer = Renderer()
+        scene = Scene()
+        camera = PerspectiveCamera(15, 1, 1, 1000)
+
         # load obj file
         loader = OBJLoader()
         obj_path = os.path.join(os.path.dirname(__file__), "./testnurbs-2.obj")
         obj = loader.load(obj_path)
 
-        scene = Scene()
         scene.add(*obj.children)
         for obj in scene.children:
             obj.pos.z = -20
@@ -35,20 +39,15 @@ class MainApp(App):
         scene.children[2].material.diffuse = 0.0, 0.0, 0.7  # blue
         scene.children[3].material.diffuse = 0.7, 0.7, 0.0  # yellow
 
-        camera = PerspectiveCamera(15, 1, 1, 1000)
-
-        renderer = self.renderer = Renderer()
-        renderer.bind(size=self._adjust_aspect)
         renderer.render(scene, camera)
-
-        root = FloatLayout()
         root.add_widget(renderer)
-        return root
 
-    def _adjust_aspect(self, inst, val):
-        rsize = self.renderer.size
-        aspect = rsize[0] / float(rsize[1])
-        self.renderer.camera.aspect = aspect
+        def _adjust_aspect(inst, val):
+            rsize = renderer.size
+            aspect = rsize[0] / float(rsize[1])
+            renderer.camera.aspect = aspect
+        renderer.bind(size=_adjust_aspect)
+        return root
 
 
 if __name__ == "__main__":
