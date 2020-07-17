f74593103ebcb24bf2fd0d89aac85c3046a7313d
Leo <lauersperg@gmx.at>
2020-07-17 09:48:43 +0200

    make _adjust_aspect class member

--- examples/createscene.py
+++ examples/createscene.py
@@ -18,7 +18,7 @@ stl_file = os.path.join(_this_path, "./test.stl")
 
 class SceneApp(App):
     def build(self):
-        renderer = Renderer(shader_file=shader_file)
+        renderer = self.renderer = Renderer(shader_file=shader_file)
         renderer.set_clear_color((0.16, 0.30, 0.44, 1.0))
 
         loader = STLLoader()
@@ -39,11 +39,7 @@ class SceneApp(App):
 
         camera = PerspectiveCamera(75, 0.3, 0.5, 1000)
 
-        def _adjust_aspect(inst, val):
-            rsize = renderer.size
-            aspect = rsize[0] / float(rsize[1])
-            renderer.camera.aspect = aspect
-        renderer.bind(size=_adjust_aspect)
+        renderer.bind(size=self._adjust_aspect)
         renderer.render(scene, camera)
 
         root = FloatLayout()
@@ -51,6 +47,11 @@ class SceneApp(App):
         Clock.schedule_interval(self._rotate_cube, 1 / 20)
 
         return root
+
+    def _adjust_aspect(self, inst, val):
+        rsize = self.renderer.size
+        aspect = rsize[0] / float(rsize[1])
+        self.renderer.camera.aspect = aspect
 
     def _rotate_cube(self, dt):
         self.cube.rotation.x += 1
