f71f96dd652d0524d282476e50996b7725dadd8b
Leo <lauersperg@gmx.at>
2020-07-20 13:36:04 +0200

    revert sphere cleanup

--- examples/sphere/main.py
+++ examples/sphere/main.py
@@ -1,32 +1,28 @@
-from kivy.app import App
-from kivy.uix.floatlayout import FloatLayout
-from kivy3 import Material
-from kivy3 import Mesh
-from kivy3 import PerspectiveCamera
-from kivy3 import Renderer
-from kivy3 import Scene
-from kivy3.extras.geometries import GridGeometry
-from kivy3.extras.geometries import SphereGeometry
-from kivy3.objects.lines import Lines
-import math
 import os
+import math
+from kivy.app import App
+from kivy.clock import Clock
+
+from kivy3 import Scene, Renderer, PerspectiveCamera, Material, Mesh
+from kivy3.extras.geometries import GridGeometry, SphereGeometry
+from kivy.uix.floatlayout import FloatLayout
+from kivy3.objects.lines import Lines
 
 # Resource paths
 _this_path = os.path.dirname(os.path.realpath(__file__))
 shader_file = os.path.join(_this_path, "./blinnphong.glsl")
 
 
-class SphereExample(App):
-    """This is a basic example showcasing the Sphere Geometry.
-    """
-
+class MainApp(App):
     def build(self):
-        renderer = Renderer(shader_file=shader_file)
-        renderer.set_clear_color((0.2, 0.2, 0.2, 1.0))
-
+        self.renderer = Renderer(shader_file=shader_file)
+        scene = Scene()
         camera = PerspectiveCamera(45, 1, 0.1, 2500)
+        self.renderer.set_clear_color((0.2, 0.2, 0.2, 1.0))
 
-        renderer.main_light.intensity = 5000
+        self.camera = camera
+        self.renderer.main_light.intensity = 5000
+        root = ObjectTrackball(camera, 10)
 
         geometry = SphereGeometry(radius=1)
         material = Material(
@@ -37,6 +33,7 @@ class SphereExample(App):
             transparency=0.8,
         )
         obj = Mesh(geometry, material)
+        scene.add(obj)
 
         # create a grid on the xz plane
         geometry = GridGeometry(size=(30, 30), spacing=1)
@@ -48,23 +45,19 @@ class SphereExample(App):
         )
         lines = Lines(geometry, material)
         lines.rotation.x = 90
-
-        scene = Scene()
-        scene.add(obj)
         scene.add(lines)
 
-        renderer.render(scene, camera)
-        renderer.main_light.intensity = 500
+        self.renderer.render(scene, camera)
+        self.renderer.main_light.intensity = 500
 
-        def _adjust_aspect(inst, val):
-            rsize = renderer.size
-            aspect = rsize[0] / float(rsize[1])
-            renderer.camera.aspect = aspect
-        renderer.bind(size=_adjust_aspect)
-
-        root = ObjectTrackball(camera, 10)
-        root.add_widget(renderer)
+        root.add_widget(self.renderer)
+        self.renderer.bind(size=self._adjust_aspect)
         return root
+
+    def _adjust_aspect(self, inst, val):
+        rsize = self.renderer.size
+        aspect = rsize[0] / float(rsize[1])
+        self.renderer.camera.aspect = aspect
 
 
 class ObjectTrackball(FloatLayout):
@@ -113,4 +106,4 @@ class ObjectTrackball(FloatLayout):
 
 
 if __name__ == "__main__":
-    SphereExample().run()
+    MainApp().run()
